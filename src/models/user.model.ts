8fdccf29894210c9269f43beb38e3c716de5ce23
EasyDev-Alex <inflammablehd@gmail.com>
2026-03-04 17:58:06 +0100

    Created login component

--- src/models/user.model.ts
+++ src/models/user.model.ts
@@ -1,5 +1,8 @@
 export interface UserModel {
-    email: string,
-    password: string,
+    firstName: string
+    lastName: string
+    email: string
+    password: string
+    destination: string
     orders: any[]
 }
