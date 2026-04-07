0064e685ade684dc4c540bb93fc017bdee9cfab5
EasyDev-Alex <inflammablehd@gmail.com>
2026-04-07 16:14:37 +0200

    Created function doLogin(), connected it to Submit button

--- src/app/login/login.ts
+++ src/app/login/login.ts
@@ -1,14 +1,17 @@
 import { Component } from '@angular/core';
 import { MatButtonModule } from '@angular/material/button';
 import { MatCardModule } from '@angular/material/card';
+import { MatIconModule } from '@angular/material/icon';
 import { MatInputModule } from '@angular/material/input';
 
 @Component({
   selector: 'app-login',
-  imports: [MatCardModule, MatInputModule, MatButtonModule],
+  imports: [MatCardModule, MatInputModule, MatButtonModule, MatIconModule],
   templateUrl: './login.html',
   styleUrl: './login.css',
 })
 export class Login {
+  doLogin() {
 
+  }
 }
