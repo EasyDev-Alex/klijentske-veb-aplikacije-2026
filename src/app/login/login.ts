906491f3b544baf27418690d7bd03f6dae626bfd
EasyDev-Alex <inflammablehd@gmail.com>
2026-04-07 15:51:34 +0200

    Added Submit button to Login page

--- src/app/login/login.ts
+++ src/app/login/login.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
+import { MatButtonModule } from '@angular/material/button';
 import { MatCardModule } from '@angular/material/card';
 import { MatInputModule } from '@angular/material/input';
 
 @Component({
   selector: 'app-login',
-  imports: [MatCardModule, MatInputModule],
+  imports: [MatCardModule, MatInputModule, MatButtonModule],
   templateUrl: './login.html',
   styleUrl: './login.css',
 })
