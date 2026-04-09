758f7854e5903cf5e55deb4c68adcd5990692fc6
EasyDev-Alex <inflammablehd@gmail.com>
2026-04-09 18:27:50 +0200

    Added fields to User Account page

--- src/app/user/user.ts
+++ src/app/user/user.ts
@@ -1,19 +1,31 @@
-import { Component } from '@angular/core';
+import { Component, signal } from '@angular/core';
 import { AuthService } from '../services/auth.service';
 import { Router } from '@angular/router';
+import { MatCardModule } from '@angular/material/card';
+import { MatInputModule } from '@angular/material/input';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
+import { FormsModule } from '@angular/forms';
+import axios from 'axios';
+import {MatSelectModule} from '@angular/material/select';
 
 @Component({
   selector: 'app-user',
-  imports: [],
+  imports: [MatCardModule, MatInputModule, MatButtonModule, MatIconModule, FormsModule, MatSelectModule],
   templateUrl: './user.html',
   styleUrl: './user.css',
 })
 export class User {
   public activeUser = AuthService.getActiveUser()
+  destinations = signal<string[]>([])
 
   constructor(private router: Router) {
     if (!AuthService.getActiveUser()) {
       router.navigate(['/login'])
+      return
     }
+
+    axios.get('https://flight.pequla.com/api/flight/destination')
+    .then(rsp => this.destinations.set(rsp.data))
   }
 }
