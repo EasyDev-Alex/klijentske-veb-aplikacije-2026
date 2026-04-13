ff35efa2acbfb43263addaafc75d27b11986a18a
EasyDev-Alex <inflammablehd@gmail.com>
2026-04-13 18:47:49 +0200

    Add Loading to all pages

--- src/app/details/details.ts
+++ src/app/details/details.ts
@@ -9,11 +9,12 @@ import { MatIconModule } from '@angular/material/icon';
 import { AuthService } from '../services/auth.service';
 import { MatButtonModule } from '@angular/material/button';
 import { FlightService } from '../services/flight.service';
+import { Loading } from '../loading/loading';
 
 
 @Component({
   selector: 'app-details',
-  imports: [MatCardModule, MatListModule, MatIconModule, RouterLink, MatButtonModule],
+  imports: [MatCardModule, MatListModule, MatIconModule, RouterLink, MatButtonModule, Loading],
   templateUrl: './details.html',
   styleUrl: './details.css',
 })
