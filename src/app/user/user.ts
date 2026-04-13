de20f2be91669880671986662c4f21e88ef80d08
EasyDev-Alex <inflammablehd@gmail.com>
2026-04-13 20:10:54 +0200

    Added alert popups

--- src/app/user/user.ts
+++ src/app/user/user.ts
@@ -6,9 +6,10 @@ import { MatInputModule } from '@angular/material/input';
 import { MatButtonModule } from '@angular/material/button';
 import { MatIconModule } from '@angular/material/icon';
 import { FormsModule } from '@angular/forms';
-import {MatSelectModule} from '@angular/material/select';
+import { MatSelectModule } from '@angular/material/select';
 import { FlightService } from '../services/flight.service';
 import { Loading } from '../loading/loading';
+import { Alerts } from '../alerts';
 
 @Component({
   selector: 'app-user',
@@ -19,6 +20,8 @@ import { Loading } from '../loading/loading';
 export class User {
   public activeUser = AuthService.getActiveUser()
   destinations = signal<string[]>([])
+  newPassword: string = ''
+  passwordRepeat: string = ''
 
   constructor(private router: Router) {
     if (!AuthService.getActiveUser()) {
@@ -27,11 +30,11 @@ export class User {
     }
 
     FlightService.getDestinations()
-    .then(rsp => this.destinations.set(rsp.data))
+      .then(rsp => this.destinations.set(rsp.data))
   }
 
   updateUser() {
     AuthService.updateActiveUser(this.activeUser!)
-    alert('User updated successfully!')
+    Alerts.success("User updated successfully!")
   }
 }
