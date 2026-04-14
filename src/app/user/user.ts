4b95a8da6dc5cbab67472861f31e7dea4a1414f9
EasyDev-Alex <inflammablehd@gmail.com>
2026-04-14 22:18:36 +0200

    Add recommended flights

--- src/app/user/user.ts
+++ src/app/user/user.ts
@@ -1,6 +1,6 @@
 import { Component, signal } from '@angular/core';
 import { AuthService } from '../services/auth.service';
-import { Router } from '@angular/router';
+import { Router, RouterLink } from '@angular/router';
 import { MatCardModule } from '@angular/material/card';
 import { MatInputModule } from '@angular/material/input';
 import { MatButtonModule } from '@angular/material/button';
@@ -10,21 +10,25 @@ import { MatSelectModule } from '@angular/material/select';
 import { FlightService } from '../services/flight.service';
 import { Loading } from '../loading/loading';
 import { Alerts } from '../alerts';
+import { FlightModel } from '../../models/flight.model';
+import { MatListModule } from '@angular/material/list';
+import { Utils } from '../utils';
 
 @Component({
   selector: 'app-user',
-  imports: [MatCardModule, MatInputModule, MatButtonModule, MatIconModule, FormsModule, MatSelectModule, Loading],
+  imports: [MatCardModule, MatInputModule, MatButtonModule, MatIconModule, FormsModule, MatSelectModule, Loading, MatListModule, RouterLink],
   templateUrl: './user.html',
   styleUrl: './user.css',
 })
 export class User {
   public activeUser = AuthService.getActiveUser()
   destinations = signal<string[]>([])
+  recommended = signal<FlightModel[]>([])
   oldPassword: string = ''
   newPassword: string = ''
   passwordRepeat: string = ''
 
-  constructor(private router: Router) {
+  constructor(private router: Router, public utils: Utils) {
     if (!AuthService.getActiveUser()) {
       router.navigate(['/login'])
       return
@@ -34,6 +38,9 @@ export class User {
 
     FlightService.getDestinations()
       .then(rsp => this.destinations.set(rsp.data))
+
+      FlightService.getFlightsToDestination(this.activeUser!.destination)
+      .then(rsp=>this.recommended.set(rsp.data.content))
   }
 
   getAvatarUrl() {
