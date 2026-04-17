80399b260c07cb0163ed6d517ab833090df4d43d
EasyDev-Alex <inflammablehd@gmail.com>
2026-04-17 19:08:12 +0200

    Added content to Order page

--- src/app/order/order.ts
+++ src/app/order/order.ts
@@ -1,11 +1,19 @@
 import { Component, signal } from '@angular/core';
 import { FlightModel } from '../../models/flight.model';
 import { DataService } from '../services/data.service';
-import { ActivatedRoute } from '@angular/router';
+import { ActivatedRoute, RouterLink } from '@angular/router';
+import { FlightService } from '../services/flight.service';
+import { MatCardModule } from '@angular/material/card';
+import { FormsModule } from '@angular/forms';
+import { MatButtonModule } from '@angular/material/button';
+import { Loading } from '../loading/loading';
+import { Utils } from '../utils';
+import { MatListModule } from '@angular/material/list';
+import { MatIconModule } from '@angular/material/icon';
 
 @Component({
   selector: 'app-order',
-  imports: [],
+  imports: [MatCardModule, RouterLink, FormsModule, MatButtonModule, Loading, MatListModule, MatIconModule],
   templateUrl: './order.html',
   styleUrl: './order.css',
 })
@@ -14,9 +22,13 @@ export class Order {
   airlines = DataService.getAirlines();
   seatingType = DataService.getSeatingTypes();
 
-  constructor (private route: ActivatedRoute) {
-    route.params.subscribe(params => {
-      console.log(params['id']);
+  constructor (private route: ActivatedRoute, public utils: Utils) {
+    this.route.params.subscribe(params => {
+      const id = Number(params['id']);
+      FlightService.getFlightById(id)
+      .then(rsp => {
+        this.flight.set(rsp.data);
+      })
     })
   }
   
